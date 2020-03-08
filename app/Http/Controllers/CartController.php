e91c0cbb29abc7c3d1fdcf483aca5d6cc5d22dd6
Precious Ibeagi <preciousaang@gmail.com>
2020-03-08 10:29:20 +0100

    cart o

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,14 +8,55 @@ use App\Course;
 class CartController extends Controller
 {
     public function list(Request $request){
-        $collection = collect($request->session()->get('cart'))->keys()->map(function($key){
-            return Course::find($key);
-        });
-        return response()->json($collection->all(), 200);
+        $cart = $request->session()->get('cart');
+        return response()->json($cart, 200);
     }
 
     public function add(Request $request){
-        $request->session()->push('cart.'.$request->post('id'), $request->post('id'));
+        $request->validate([
+            'id' => 'required'
+        ]);
+
+        $course = Course::find($request->post('id'));
+
+        $itemArray = array(
+            $course->id => array(
+                'id' => $course->id,
+                'name' => $course->title,
+                'quantity' => 1,
+                'image' => asset(config('paths.course_images').$course->image),
+                'price' => $course->price,
+            )
+        );
+
+        if(!empty(session()->get('cart'))){
+            $cartItems = session()->get('cart');
+            //check if the course is already in the cart
+            if(in_array($course->id, array_keys($cartItems))){
+                foreach($cartItems as $key => $val){
+                    if($course->id == $key){
+                        // check if theres any quantity
+                        // if(empty($cartItems[$key]["quantity"])){
+                        //     //if there's none initilize as 0 to avoid integer error
+                        //     $cartItems[$key]["quantity"] = 0;
+                        // }
+                        $cartItems[$key]['quantity'] += 1;
+                    }
+                }
+            }else{
+                //it isn't in the cart yet
+
+                //use the union operator instead of the array_merge to avoid renumbering of indices
+                $cartItems = $cartItems + $itemArray;
+            }
+            //Finally throw it in the cart
+            session(['cart'=>$cartItems]);
+        }else{
+            session(['cart'=>$itemArray]);
+        }
+
+
+        // $request->session()->push('cart.'.$request->post('id'), $request->post('id'));
         return response()->json("Ok", 200);
     }
 
